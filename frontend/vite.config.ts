06efa06098e5ff38ef5d5bb1622a6409c39cb2ea
Riley Flynn <riley@rileyflynn.me>
2024-04-21 10:46:11 -0230

    Add base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,6 @@ import type { UserConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default {
+    base: '/conjam/',
     plugins: [tsconfigPaths()],
 } satisfies UserConfig;
